8ac4ff2ea63317bb0d64a152c263d8038880100b
sebasdx <sebasactis@gmail.com>
2024-05-04 06:07:45 -0300

    ajustes

--- src/Components/contactPage.js
+++ src/Components/contactPage.js
@@ -1,38 +1,24 @@
-import React, { useState } from 'react';
 import './css/ContactPage.css';
 
 function ContactPage() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  };
-
+  
   return (
     <div className="contact-page">
       <h1 className='titulo-contacto'>Contacto</h1>
       <div className="contact-form">
         <h2 className='subtitulo-contacto'>Envíanos un mensaje</h2>
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="form-group">
             <label htmlFor="name">Nombre:</label>
-            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+            <input type="text" id="name" name="name" required />
           </div>
           <div className="form-group">
             <label htmlFor="email">Correo electrónico:</label>
-            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+            <input type="email" id="email" name="email"  required />
           </div>
           <div className="form-group">
             <label htmlFor="message">Mensaje:</label>
-            <textarea id="message" name="message" value={formData.message} onChange={handleChange} rows="4" required></textarea>
+            <textarea id="message" name="message"  required></textarea>
           </div>
           <button type="submit">Enviar</button>
         </form>
